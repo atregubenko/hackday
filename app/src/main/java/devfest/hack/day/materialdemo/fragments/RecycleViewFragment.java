f55d9dbdc905084e746156e8dfdb23e926cdbe3d
Anna Tregubenko <anna@cleverua.com>
2014-10-24 14:07:41 +0300

    added some resources

--- app/src/main/java/devfest/hack/day/materialdemo/fragments/RecycleViewFragment.java
+++ app/src/main/java/devfest/hack/day/materialdemo/fragments/RecycleViewFragment.java
@@ -3,6 +3,7 @@ package devfest.hack.day.materialdemo.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -43,5 +44,8 @@ public class RecycleViewFragment extends Fragment {
         String[] myDataset = new String[] {"one", "two", "three", "four", "five"};
         mAdapter = new CustomAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
+
+        RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
+        mRecyclerView.setItemAnimator(itemAnimator);
     }
 }
